40d11353eaad4bd9a8466f85ad696257fe7280a1
Igor Seberino <igorseberino@gmail.com>
2026-03-10 23:40:42 -0300

    feat: Implementação da autenticação JWT

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserViewSet
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
     path("", include(router.urls)),
 ]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,6 +139,9 @@ AUTH_USER_MODEL = "accounts.User"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
 }
 
 # CORS
