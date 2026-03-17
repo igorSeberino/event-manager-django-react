88fb15362c0f44f655cf8671695096e58bcfe349
Igor Seberino <igorseberino@gmail.com>
2026-03-17 19:06:41 -0300

    feat: Implementação da autenticação no app Registrations

--- backend/registrations/views.py
+++ backend/registrations/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Registration
 from .serializers import RegistrationSerializer
@@ -7,3 +8,7 @@ from .serializers import RegistrationSerializer
 class RegistrationViewSet(viewsets.ModelViewSet):
     queryset = Registration.objects.all()
     serializer_class = RegistrationSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
