666909cc8f31b3abec58bcffc702bffc6d449df3
Igor Seberino <igorseberino@gmail.com>
2026-03-17 19:12:54 -0300

    feat: Implementação de regra de negócio que impede inscrições duplicadas

--- backend/registrations/serializers.py
+++ backend/registrations/serializers.py
@@ -7,3 +7,12 @@ class RegistrationSerializer(serializers.ModelSerializer):
         model = Registration
         fields = "__all__"
         read_only_fields = ["id", "registered_at"]
+
+    def validate(self, data):
+        user = data.get("user")
+        event = data.get("event")
+        if Registration.objects.filter(user=user, event=event).exists():
+            raise serializers.ValidationError(
+                "O usuário já está registrado para este evento."
+            )
+        return data
