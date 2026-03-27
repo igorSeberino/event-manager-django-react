5b0a72429c784e3ae1554465bb219c4093bf0c82
Igor Seberino <igorseberino@gmail.com>
2026-03-27 13:18:07 -0300

    feat: Implementação de custom middleware para autenticação

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "config.middleware.TokenRefreshMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
